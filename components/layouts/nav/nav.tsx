4037599d15a66b71b8ab64fc1305a1f9a26335bc
lee44 <jlee7772@gmail.com>
2023-02-07 18:32:55 -0800

    Forgot to fix nav file

--- components/layouts/nav/nav.tsx
+++ components/layouts/nav/nav.tsx
@@ -1,13 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
-import DarkModeToggler from '../../elements/darkmodetoggler'
-
-const Nav = () => {
-  return (
-    <nav className='container h-14 flex justify-between items-center text-white'>
-      <h3 className='text-primary dark:text-white'>NextNewegg</h3>
-      <DarkModeToggler />
-=======
 import Profile from '../../elements/profile'
 
 const Nav = () => {
@@ -15,7 +6,6 @@ const Nav = () => {
     <nav className='container h-15 top-0 flex justify-between items-center text-white'>
       <h2>NextNewegg</h2>
       <Profile />
->>>>>>> ff51502a41ed150167fdde086beca8c6da03615e
     </nav>
   )
 }
