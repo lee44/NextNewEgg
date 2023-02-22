5eab6cb09d80f7815d17689a63709dfbfeee4092
lee44 <jlee7772@gmail.com>
2023-02-22 15:58:56 -0800

    Added more comments to _app.tsx. Profile or Admin page don't redirect when session expires

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,10 +6,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider
       session={pageProps.session}
-      // Re-fetch session every 60s
-      // refetchInterval={0}
-      // Re-fetches session when window is focused
-      // refetchOnWindowFocus={true}
+      // Re-fetch session every 5 minutes
+      // refetchInterval={5 * 60}
+      // Disable Re-fetches session when window is focused
+      // refetchOnWindowFocus={false}
     >
       <Component {...pageProps} />
     </SessionProvider>
