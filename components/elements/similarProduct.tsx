8f8ec8d303d0b3bfe3814f9025886804fce8560d
lee44 <jlee7772@gmail.com>
2023-03-14 16:21:47 -0700

    Added Links to each similar product

--- components/elements/similarProduct.tsx
+++ components/elements/similarProduct.tsx
@@ -2,15 +2,18 @@ import React from 'react'
 import Image from 'next/image'
 import { ProductBase } from '../../types/product'
 import StarRating from './starrating'
+import Link from 'next/link'
 
 const SimilarProduct = ({ product }: ProductBase) => {
   return (
-    <div className='flex flex-col items-center gap-3 w-[200px]'>
-      {/* <Image src={product?.img ?? ''} alt='product' className='object-contain' fill /> */}
-      <Image src={'/no-image.png'} alt='product' className='object-contain' width={150} height={150} />
-      <StarRating stars={product.stars} />
-      <h6>{product.name}</h6>
-    </div>
+    <Link href={`/product/${product.productId}`}>
+      <div className='flex flex-col items-center gap-3 w-[200px]'>
+        {/* <Image src={product?.img ?? ''} alt='product' className='object-contain' fill /> */}
+        <Image src={'/no-image.png'} alt='product' className='object-contain' width={150} height={150} />
+        <StarRating stars={product.stars} />
+        <h6>{product.name}</h6>
+      </div>
+    </Link>
   )
 }
 
