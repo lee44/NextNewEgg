fa69e08a8913467f32a1ca67f4c049805c1e3e12
lee44 <jlee7772@gmail.com>
2023-04-05 11:47:35 -0700

    Finished userCard

--- components/dashboard/overview/userCard.tsx
+++ components/dashboard/overview/userCard.tsx
@@ -4,20 +4,23 @@ import Card from '../../templates/card'
 import { RxAvatar } from 'react-icons/rx'
 
 const UserCard = ({ user }: { user: User }) => {
-  console.log(typeof user.created_at)
+  const created_at = new Date(user.created_at)
 
   return (
     <Card>
-      <div className='flex items-center gap-4'>
+      <div className='flex items-center justify-between gap-4'>
         <RxAvatar color='white' size={50} />
         <div>
-          <h6>{user.name}</h6>
-          <p>
-            {user.email} {user.role}
-          </p>
+          <h5 className='text-left'>{user.name}</h5>
+          <p>{user.email}</p>
+          <p>{user.role}</p>
         </div>
         <div>
-          <h6>{user.created_at.toString().substring(0, 10)}</h6>
+          <h6>
+            {created_at.toLocaleDateString()}
+            <br />
+            {created_at.toLocaleTimeString()}
+          </h6>
         </div>
       </div>
     </Card>
